c691ced473707744fb03fdb40b22ba25c8460070
jaequery <jaequery@gmail.com>
2018-03-20 19:46:17 -0700

    added Api controller

--- config.ru
+++ config.ru
@@ -2,6 +2,7 @@ require './config'
 
 # set controllers
 map('/admin') { run AdminApp }
+map('/api') { run ApiApp }
 map('/') { run SiteApp }
 
 
--- controllers/site.rb
+++ controllers/site.rb
@@ -3,11 +3,6 @@ class SiteApp < App
   get '/' do    
     erb :index
   end
-
-  get '/api/users' do
-    content_type :json
-    output({:name => 'jame'}, 404)
-  end
   
 end
 
