b2ec06bbb8374f6c550e17e711cd48ccda35cb44
jaequery <jaequery@gmail.com>
2018-03-25 22:44:01 -0700

    removed un-needed helpers

--- lib/sinatra/jasis/helpers.rb
+++ lib/sinatra/jasis/helpers.rb
@@ -3,26 +3,7 @@ require 'sinatra/base'
 module Sinatra
   module Jasis
 
-    
-      # outputs json response with http status code 
-      def output(obj, code = 200)      
-        halt code, obj.to_json if obj.present?
-        return false
-      end
-
-      # email using pony
-      def email(data, template = nil)
-        if template.present?
-          file = template[:file]
-          liquid = Liquid::Template.parse(File.read(file))
-          body = liquid.render(template[:data])
-          data[:body] = body
-        end
-        Pony.mail(data)
-      end
-
-
-    
+        
   end
 
   helpers Jasis
