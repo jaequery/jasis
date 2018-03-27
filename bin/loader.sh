aaf918a8e2c9d10e742537d504b068b54d2a1ac0
jaequery <jaequery@gmail.com>
2018-03-27 13:21:29 -0700

    updated loader of bin files

--- bin/loader.sh
+++ bin/loader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 parentdir="$(dirname "$dir")"
-app=${parentdir##*/} 
-app="${app//-}"
+app_path=${parentdir##*/} 
+app="${app_path//-}"
 
 
