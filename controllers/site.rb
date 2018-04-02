435a573d3eb470dcde8b7b65d8f25c336fcee2cb
jaequery <jaequery@gmail.com>
2018-04-02 00:02:10 -0700

    added bridge networking mode for production to accomodate docker-compose version 2+

--- controllers/site.rb
+++ controllers/site.rb
@@ -13,6 +13,22 @@ class SiteApp < App
     abort
   end
 
+  get '/auth/facebook/callback' do
+    auth = request.env["omniauth.auth"]
+    user = User.login_with_omniauth(auth)
+
+    if user
+      session[:user_id] = user[:id]        
+      if user.email.blank?
+        redirect("/login", :notice => 'Please fill in required informations')
+      end
+
+      redirect("/")
+    else
+      output(user.values)
+    end
+  end
+
 
 end
 
